77835f383fa6a05325b457bde9957fc9e6a41a44
Ianto Lin Xi <ianto@kite.com>
2018-05-06 02:43:27 -0400

    build

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,9 @@
 'use strict'
+const credentials = require('./.credentials.json')
+
 module.exports = {
-  NODE_ENV: '"production"'
+  NODE_ENV: '"production"',
+  CTF_SPACE_ID: '"' + credentials.CTF_SPACE_ID + '"',
+  CTF_CDA_ACCESS_TOKEN: '"' + credentials.CTF_CDA_ACCESS_TOKEN + '"',
+  MIXPANEL_TOKEN: '"' + credentials.MIXPANEL_TOKEN + '"',
 }
