d75ac28ef6e5ce6cf72cbee4f80472343ad2a363
Ianto Lin Xi <ianto@kite.com>
2018-05-06 02:48:08 -0400

    remove from dev

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,11 +1,7 @@
 'use strict'
 const merge = require('webpack-merge')
 const prodEnv = require('./prod.env')
-const credentials = require('./.credentials.json')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  CTF_SPACE_ID: '"' + credentials.CTF_SPACE_ID + '"',
-  CTF_CDA_ACCESS_TOKEN: '"' + credentials.CTF_CDA_ACCESS_TOKEN + '"',
-  MIXPANEL_TOKEN: '"' + credentials.MIXPANEL_TOKEN + '"',
 })
