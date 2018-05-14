668578e3c94465c361761ee93d70c0f8569676a2
Ianto Lin Xi <ianto@kite.com>
2018-05-13 22:33:31 -0400

    add prod keys

--- config/prod.env.js
+++ config/prod.env.js
@@ -6,4 +6,5 @@ module.exports = {
   CTF_SPACE_ID: '"' + credentials.CTF_SPACE_ID + '"',
   CTF_CDA_ACCESS_TOKEN: '"' + credentials.CTF_CDA_ACCESS_TOKEN + '"',
   MIXPANEL_TOKEN: '"' + credentials.MIXPANEL_TOKEN + '"',
+  SEGMENT_TOKEN: '"' + credentials.SEGMENT_TOKEN + '"',
 }
